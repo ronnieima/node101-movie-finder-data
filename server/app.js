5a8c07f2afca8cffda30dea594fef3ba83bbea4e
Ronnie Kaito Imagawa <ronnie.imagawa@cccareers.org>
2024-09-18 16:19:40 -0400

    finish

--- server/app.js
+++ server/app.js
@@ -1,8 +1,23 @@
-const express = require('express');
+const axios = require("axios");
+const express = require("express");
+const morgan = require("morgan");
 
+require("dotenv").config();
 
 const app = express();
+const API_KEY = process.env.API_KEY;
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+app.use(morgan("dev"));
+
+app.get("/", async (req, res) => {
+  try {
+    const movie = await axios.get(`http://www.omdbapi.com?apikey=${API_KEY}`, {
+      params: req.query,
+    });
+    res.json(movie.data).status(200);
+  } catch (error) {
+    throw error;
+  }
+});
 
 module.exports = app;
